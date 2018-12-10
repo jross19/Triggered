052acf0a1af10d86eafcb0e7f39d613bf5061f10
jross19 <jross19@stjohnsprep.org>
2018-12-10 17:42:52 -0500

    Initial Commit

---
+++ Triggered/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Triggered
+//
+//  Created by Jonathan Ross on 12/10/18.
+//  Copyright © 2018 Jonathan Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
