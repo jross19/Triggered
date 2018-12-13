ac5e95a5a612c7c883114dd8a1911f192ba1c4fe
Jonathan Ross <jross19@k132-24.stjohnsprep.org>
2018-12-13 12:56:18 -0500

    commit

--- Triggered/ViewController.swift
+++ Triggered/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 import SwiftSoup //This is a module for Web Scraping
+import Alamofire
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         
+    }
+    
+    // Grabs the HTML from nycmetalscene.com for parsing.
+    func scrapeNYCMetalScene() -> Void {
+        Alamofire.request("http://nycmetalscene.com").responseString { response in
+            print("\(response.result.isSuccess)")
+            if let html = response.result.value {
+                self.parseHTML(html: html)
+            }
+        }
+    }
+    
+    func parseHTML(html: String) -> Void {
+        // Finish this next
     }
     
 }
