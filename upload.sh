e76f555f2e6d77ad93b4d3260d2a70cf1e7e1f11
Jonathan Ross <jross19@sl108-14.stjohnsprep.org>
2019-01-08 14:10:34 -0500

    Use this script to upload the command is ./upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -p "Commit description: " desc
+read -p "Please give a commit description: " desc
 git add -A && \
 git commit -m "$desc" && \
 git push -u origin master && \
-echo "Script has run successfully "
+echo "Script has run successfully"
